7f084e985ec8c29e174d9ff27a7c7332d2169b87
Raushan Kumar <raushan2022excellent@gamil.com>
2026-01-29 18:47:39 +0530

    add Admin Server

--- Vehicle-Service/src/main/java/com/garage/Vehicle_Service/service/VehicleService.java
+++ Vehicle-Service/src/main/java/com/garage/Vehicle_Service/service/VehicleService.java
@@ -6,6 +6,7 @@ import com.garage.Vehicle_Service.dto.VehicleDto;
 import com.garage.Vehicle_Service.entity.Vehicle;
 import com.garage.Vehicle_Service.repo.VehicleRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -22,31 +23,68 @@ public class VehicleService {
     @Autowired
     private CustomerFeignClient customerFeignClient;
 
-    public List<VehicleDto> vehicleRegistration(Map<Long,Vehicle> vehicles){
+    @Autowired
+    private Environment env;
+
+    public String customerPort( ){
+
+        return customerFeignClient.getCustomerPort();
+    }
+
+    //Register the vehicle and return vehicleDto
+    public List<VehicleDto> vehicleRegistration(List<Vehicle> vehicles){
 
         List<VehicleDto> vehicleDTos=new ArrayList<>();
 
-        for (Map.Entry<Long,Vehicle> vehicleEntry: vehicles.entrySet()){
-            Vehicle vehicle =vehicleEntry.getValue();
+        for (Vehicle vehicle: vehicles){
             VehicleDto vehicleDto=new VehicleDto();
 
-            if (vehicle != null){
+            if (vehicle == null){
+                throw new RuntimeException("Vehicle Not find");
+
+            } else {
                 vehicleRepo.save(vehicle);
-                vehicleDto.setCustomerId(vehicle.getVehicleId());
+                vehicleDto.setVehicleId(vehicle.getVehicleId());
+                vehicleDto.setCustomerId(vehicle.getCustomerId());
                 vehicleDto.setBrand(vehicle.getBrand());
                 vehicleDto.setFuelType(vehicle.getFuelType());
                 vehicleDto.setManufactureYear(vehicle.getManufactureYear());
                 vehicleDto.setModel(vehicle.getModel());
                 vehicleDto.setRegistrationNumber(vehicle.getRegistrationNumber());
-                vehicleDto.setVehicleId(vehicle.getVehicleId());
                 vehicleDto.setServiceStatus(vehicle.getServiceStatus());
-                vehicleDto.setNumberOfWheels(vehicleDto.getNumberOfWheels());
+                vehicleDto.setNumberOfWheels(vehicle.getNumberOfWheels());
                 vehicleDTos.add(vehicleDto);
             }
         }
 
         return vehicleDTos;
+
     }
 
+    //Find vehicle by customerId and return list vehicleDto
+    public List<VehicleDto> findVehicleByCustomerId(Long cusId){
+        List<Vehicle> vehicles=vehicleRepo.findByCustomerId(cusId);
+        if (vehicles == null){
+            throw new RuntimeException("Vehicle not exist with this customerId");
+        }
 
+        List<VehicleDto> vehicleDTos=new ArrayList<>();
+
+        for (Vehicle vehicle: vehicles) {
+            VehicleDto vehicleDto = new VehicleDto();
+
+            vehicleDto.setVehicleId(vehicle.getVehicleId());
+            vehicleDto.setCustomerId(vehicle.getCustomerId());
+            vehicleDto.setBrand(vehicle.getBrand());
+            vehicleDto.setFuelType(vehicle.getFuelType());
+            vehicleDto.setManufactureYear(vehicle.getManufactureYear());
+            vehicleDto.setModel(vehicle.getModel());
+            vehicleDto.setRegistrationNumber(vehicle.getRegistrationNumber());
+            vehicleDto.setServiceStatus(vehicle.getServiceStatus());
+            vehicleDto.setNumberOfWheels(vehicle.getNumberOfWheels());
+            vehicleDTos.add(vehicleDto);
+        }
+
+        return vehicleDTos;
+    }
 }

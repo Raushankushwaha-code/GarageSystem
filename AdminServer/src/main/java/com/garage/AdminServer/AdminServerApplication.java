4e9d6d54ba59a34d8dabd743ce8064be9dfae896
Raushan Kumar <raushan2022excellent@gamil.com>
2026-01-30 19:52:47 +0530

    add GlobalExceptionHandler

--- AdminServer/src/main/java/com/garage/AdminServer/AdminServerApplication.java
+++ AdminServer/src/main/java/com/garage/AdminServer/AdminServerApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AdminServerApplication {
 
+	//Admin Server
 	public static void main(String[] args) {
 		SpringApplication.run(AdminServerApplication.class, args);
 	}
